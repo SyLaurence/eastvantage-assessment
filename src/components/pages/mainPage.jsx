fd1bb9d40f559b20196a549e3f7c98740c2af20f
Laurence Ivan Sy <sylaurence1997@gmail.com>
2024-02-15 15:05:42 +0800

    chore: setup initial files and folder structure

---
+++ src/components/pages/mainPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import UserSection from '../ui/userSectionComponent'
+
+export default function mainPage() {
+  return (
+    <main>
+        <UserSection />
+    </main>
+  )
+}
---
+++ src/components/ui/userSectionComponent.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function userSectionComponent() {
+  return (
+    <div>userSectionComponent</div>
+  )
+}
