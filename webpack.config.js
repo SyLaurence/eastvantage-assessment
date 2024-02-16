1ff4cd94adb9310ba2ba89f1b6ec7081c010f5f7
Laurence Ivan Sy <sylaurence1997@gmail.com>
2024-02-16 14:14:30 +0800

    chore: Updated webpack for production build

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, "dist"),
         filename: "index_bundle.js",
-        publicPath: "/"
+        publicPath: "./"
     },
     module: {
         rules: [
@@ -18,7 +18,9 @@ module.exports = {
     resolve: {
         extensions: [".jsx", "..."]
     },
-    mode: 'development',
+    mode: process.env.NODE_ENV === 'production' 
+    ? 'production' 
+    : 'development',
     plugins: [
         new HtmlWebpackPlugin({
             template: "src/index.html"
